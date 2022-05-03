a452f28fae891d0db402992639ab9d2eacfe2749
Evel Ortega <102565878+EvelOrtega@users.noreply.github.com>
2022-05-03 19:57:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const loginModal = document.getElementById("j-login-modal");
 const $simpleCarousel = document.querySelector(".js-carousel--simple");
-
+const cadastroModal = document.getElementById("modal-cadastro-overlay");
+const criarContaButton = document.getElementById("criar-conta");
+const buttonCloseCadastroModal = document.getElementById(
+  "close-cadastro-modal-button"
+);
 //modal login
 function toggleModal() {
   console.log("toggle modal");
@@ -9,6 +13,11 @@ function toggleModal() {
 function closeModal() {
   console.log("close modal");
   loginModal.classList.remove("open-modal");
+}
+function toggleCadastro() {
+  console.log("toggle cadastro");
+  cadastroModal.classList.toggle("display-none");
+  buttonCloseCadastroModal.classList.toggle("display-none");
 }
 
 //carousel
@@ -25,5 +34,6 @@ new Glider($simpleCarousel, {
 });
 
 //events
-
+criarContaButton.addEventListener("click", toggleCadastro);
+buttonCloseCadastroModal.addEventListener("click", toggleCadastro);
 window.onscroll = closeModal;
